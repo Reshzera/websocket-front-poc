eb808e9fed6375ceece4d5a6b54be869f91a6089
Reshzera <reoshiro13@gmail.com>
2025-07-15 17:01:04 -0400

    feac: change auth_token

--- src/config/websocket/socket.ts
+++ src/config/websocket/socket.ts
@@ -1,12 +1,12 @@
 // src/services/socket.ts
 import { Client } from "@stomp/stompjs";
 
-const AUTH_TOKEN = "token-auth";
+const AUTH_TOKENS = "token-auth";
 
 const createStompClient = () => {
   const client = new Client({
     webSocketFactory: () =>
-      new WebSocket(`ws://localhost:8080/ws?token=${AUTH_TOKEN}`),
+      new WebSocket(`ws://localhost:8080/ws?token=${AUTH_TOKENS}`),
     reconnectDelay: 5000,
     heartbeatIncoming: 4000,
     heartbeatOutgoing: 4000,
