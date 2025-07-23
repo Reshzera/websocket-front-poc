891811e14a550f8aa4c1bcc653c47732be8797d0
Reshzera <reoshiro13@gmail.com>
2025-07-23 10:04:19 -0400

    feat: add token auth

--- src/config/websocket/socketIo.ts
+++ src/config/websocket/socketIo.ts
@@ -1,8 +1,13 @@
 // src/services/socket.ts
 import { io, Socket } from "socket.io-client";
 
+const AUTH_TOKENS = "token-auth";
+
 const createSocketClient = () => {
   const socket: Socket = io("http://localhost:8081", {
+    query: {
+      token: AUTH_TOKENS,
+    },
     transports: ["websocket"],
     reconnectionDelay: 5000,
   });
